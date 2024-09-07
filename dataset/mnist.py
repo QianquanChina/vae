d5fcd025920f2b9549f9d296c1326e70715290a1
Jc <370047797@qq.com>
2024-09-07 16:49:55 +0800

    company commit for datasets

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -1,14 +1,17 @@
+from pathlib import Path
 from torch.utils.data import Dataset
-from typing import  Callable, Optional
+from typing import Callable, Optional
 
 
 class MnistDataset(Dataset):
 
-    def __init__(self, image_dir, transformer: Optional[Callable] = None):
-        self.image_dir = image_dir
+    def __init__(self, image_dir: str, transformer: Optional[Callable] = None):
+        image_suffix = ['.png', '.jpg']
+        self.transformer = transformer
+        self.images_path = [f for f in Path(image_dir).iterdir() if f.suffix in image_suffix]
 
-    def __getitem__(self, item):
+    def __getitem__(self, index):
         pass
 
     def __len__(self):
-        pass
+        return len(self.images_path)
